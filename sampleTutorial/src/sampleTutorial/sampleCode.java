144972ac9b65a3dae1df428f0d955a0a5010ebaa
lesliegabs <mleslie.gabriel@gmail.com>
2017-04-20 19:10:41 +0800

    Dragged the files
    The first version of code uploaded in master branch.

---
+++ sampleTutorial/src/sampleTutorial/sampleCode.java
@@ -0,0 +1,11 @@
+package sampleTutorial;
+
+public class sampleCode {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!  This is a sample code. Version 1");
+
+	}
+
+}
